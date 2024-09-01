de44ad675c018d8739adc1a8e6b066f18fa6fd42
RHAPSODY <rhapsodyquame@gmail.com>
2024-09-01 11:19:24 +0100

    made some changes

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import FormikForm from './components/formikForm';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+      <div className="App">
+      <h1>Registration Form</h1>
+      <FormikForm />
+    </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
